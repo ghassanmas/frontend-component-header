e2ed4551b53fa96e04e1e05a59fc68b0ac1c171e
Ghassan Maslamani <ghassan.maslamani@gmail.com>
2022-04-05 22:08:27 +0300

    add build

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 import Header from './Header';
-import LearningHeader from './Header';
+import LearningHeader from './learning-header/LearningHeader';
 import messages from './i18n/index';
 export { LearningHeader, messages };
 export default Header;
