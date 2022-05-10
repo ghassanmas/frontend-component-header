16d43c932271dfee91087f9bd9f10f42a19ab0b9
Ghassan Maslamani <ghassan.maslamani@gmail.com>
2022-05-10 12:17:03 +0300

    add dist

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-import Header from './Header';
-import LearningHeader from './learning-header/LearningHeader';
-import messages from './i18n/index';
-export { LearningHeader, messages };
-export default Header;
-//# sourceMappingURL=index.js.map
